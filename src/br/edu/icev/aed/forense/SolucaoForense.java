6f081ce6396b050a006729445493b7fc5e5af5cc
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-22 03:18:49 -0300

    Criado classe para ler o arquivo com todos os atributos e um método

--- src/br/edu/icev/aed/forense/SolucaoForense.java
+++ src/br/edu/icev/aed/forense/SolucaoForense.java
@@ -10,6 +10,50 @@ import java.util.Optional;
 import java.util.Set;
 
 public class SolucaoForense implements AnaliseForenseAvancada {
+    private static class LogEvent {
+        long timestamp;
+        String userId;
+        String sessionId;
+        String action;
+        String resource;
+        int severity;
+        long bytes;
+
+        LogEvent(long timestamp, String userId, String sessionId,
+                 String action, String resource, int severity, long bytes) {
+            this.timestamp = timestamp;
+            this.userId = userId;
+            this.sessionId = sessionId;
+            this.action = action;
+            this.resource = resource;
+            this.severity = severity;
+            this.bytes = bytes;
+        }
+    }
+
+    private List<LogEvent> lerLogs(String caminho) throws IOException {
+        List<LogEvent> logs = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha = br.readLine();
+            while ((linha = br.readLine()) != null) {
+                String[] c = linha.split(",");
+
+                long timestamp = Long.parseLong(c[0]);
+                String user = c[1];
+                String session = c[2];
+                String action = c[3];
+                String resource = c[4];
+                int severity = Integer.parseInt(c[5]);
+                long bytes = Long.parseLong(c[6]);
+
+                logs.add(new LogEvent(timestamp, user, session, action, resource, severity, bytes)));
+            }
+        }
+        return logs;
+    }
+
+
     @Override
     public Set<String> encontrarSessoesInvalidas(String s) throws IOException {
         return Set.of();
