480c7e7b9f24801852325f12ef43ac87405a9a73
Manoel <131733327+PlanT50@users.noreply.github.com>
2025-11-24 21:33:00 -0300

    Teste 4 e teste 5 adicionados (Fase de Testes)

--- src/br/edu/icev/aed/forense/Main.java
+++ src/br/edu/icev/aed/forense/Main.java
@@ -1,20 +1,23 @@
 package br.edu.icev.aed.forense;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         SolucaoForense sf = new SolucaoForense();
 
-        List<Alerta> top = sf.priorizarAlertas("teste3.csv", 50);
+        // o arquivo deve estar na raiz do projeto!!
+        String caminho = "teste4.csv";
 
-        for (Alerta a : top) {
-            System.out.println(a);
+        Map<Long, Long> picos = sf.encontrarPicosTransferencia(caminho);
+
+        System.out.println("==== RESULTADO DO DESAFIO 4 ====");
+        for (Map.Entry<Long, Long> e : picos.entrySet()) {
+            System.out.println("Evento em " + e.getKey() +
+                    " → próximo evento maior em " + e.getValue());
         }
 
+        System.out.println("\nFinalizado.");
     }
-    }
-
+}
